bf05ad7f1ef57dc8425bde9af4fdd1862b63cbf7
pjh <as2248@naver.com>
2022-09-26 11:02:40 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div>안녕 세상</div>
+      <div>Hello World</div>
+      <div>옹</div>
+      <div>옹옹</div>
+      <div>옹옹옹</div>
+      <div>옹옹옹옹</div>
+      <div>옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹</div>
+      <div>옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹옹</div>
+    </>
+  );
+};
+
+export default Home;
